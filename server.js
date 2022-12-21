908eb7c97c2a0d884112f8d9714177e276032071
Thiago Fernando Rech <48074835+ThiagoRech1997@users.noreply.github.com>
2022-12-21 15:08:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./src/routes')
 const cors = require('cors')
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://10.51.240.109:27017/agro-iot", { useNewUrlParser: true });
+mongoose.connect("mongodb://172.16.0.101:27017/agro-iot", { useNewUrlParser: true });
 
 const app = express()
 
